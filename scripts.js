3364171dc755de3599beaae7fbc39bed05775cd3
Aiden McNaughton <aiden-99@hotmail.co.uk>
2021-11-04 13:53:28 +0000

    Add create player function and basic functionality for appending sign to game board

--- scripts.js
+++ scripts.js
@@ -1,3 +1,42 @@
+
+function createPlayer(sign) {
+    const getSign = () => sign
+    return { getSign }
+}
+
 const gameBoard = (() => {
-    const board = ["x", "o", "x", "o", "x", "o", "x", "o", "x"]
+    let gameSquare = document.querySelectorAll(".square");
+
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    const updateBoard = (index, sign) => {
+        board[index] = sign;
+        gameSquare[index].textContent = sign;
+    }
+
+    return { updateBoard }
+
+})();
+
+const displayController = (() => {
+    let gameSquare = document.querySelectorAll(".square");
+
+    gameSquare.forEach(square => {
+        square.addEventListener("click", function () {
+            const index = square.dataset.index;
+            gameBoard.updateBoard(index, players.sign());
+        })
+    })
+
+})();
+
+const players = (() => {
+    const playerX = createPlayer("X");
+    const playerO = createPlayer("O");
+
+    return {
+        sign: () => {
+            return playerX.getSign()
+        }
+    }
 })();
