7c15a4aea0c014aa0e635b3f7851fa0a4a625aa5
Aiden McNaughton <aiden-99@hotmail.co.uk>
2021-11-05 11:51:17 +0000

    Add more functionality, game can now be played at a very basic level

--- scripts.js
+++ scripts.js
@@ -4,6 +4,23 @@ function createPlayer(sign) {
     return { getSign }
 }
 
+
+const gameController = (() => {
+    const playerX = createPlayer("X");
+    const playerO = createPlayer("O");
+
+    let count = -1;
+
+    const getCurrentSign = () => {
+        count++;
+        if (count % 2 === 0) return playerX.getSign()
+        return playerO.getSign()
+    }
+
+    return { getCurrentSign }
+})();
+
+
 const gameBoard = (() => {
     let gameSquare = document.querySelectorAll(".square");
 
@@ -18,25 +35,16 @@ const gameBoard = (() => {
 
 })();
 
+
 const displayController = (() => {
     let gameSquare = document.querySelectorAll(".square");
 
     gameSquare.forEach(square => {
         square.addEventListener("click", function () {
+            if (square.textContent != "") return;
             const index = square.dataset.index;
-            gameBoard.updateBoard(index, players.sign());
+            gameBoard.updateBoard(index, gameController.getCurrentSign());
         })
     })
 
-})();
-
-const players = (() => {
-    const playerX = createPlayer("X");
-    const playerO = createPlayer("O");
-
-    return {
-        sign: () => {
-            return playerX.getSign()
-        }
-    }
 })();
